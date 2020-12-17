aeac752a810d9ca45037ba5baea00a56c43f5c60
DUSTINS-PC\Dustin <DRush2003@hotmail.com>
2020-12-17 18:12:00 -0500

    stuff

--- s2.h
+++ s2.h
@@ -2,5 +2,5 @@
 #define word_728C_user 0xA480
 #define Obj5F_MapUnc_7240 0xA484
 #define off_3A294 0x9F8C6
-#define MapRUnc_Sonic 0x1F86A6
+#define MapRUnc_Sonic 0x1F8946
 /* Ende Includefile für C-Programm */
